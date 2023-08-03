f6659f8e27f4295b79f727e17bacf0c048b21fda
sunflowrr <s3978128@rmit.edu.vn>
2023-08-03 15:15:36 +0700

    added responsive navbar and hero

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+
+    var menu = document.querySelector('#nav-list');
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+
+}
+
+function resetNavbar() {
+    const navbar = document.querySelector('#nav-list');
+    navbar.style.display = 'flex';
+    navbar.style.flexDirection = 'row';
+}
+
+function checkScreenSize() {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        // Apply effects for smaller screens
+        toggleMenu();
+    } else {
+        // Apply effects for larger screens
+        resetNavbar();
+    }
+}
+
+// Initial check on page load
+checkScreenSize();
+
+// Listen for the resize event and reset effects when the screen size changes
+window.addEventListener('resize', checkScreenSize);
