4c3363a61a3af44437dcf46c6831c2af062ebd96
sunflowrr <s3978128@rmit.edu.vn>
2023-08-08 01:34:13 +0700

    add books to category page, add contact page

--- js/script.js
+++ js/script.js
@@ -1,35 +1,18 @@
+/*Header scroll effect*/
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 80);
 });
 
-function toggleMenu() {
+/*Carousel*/
 
-    var menu = document.querySelector('#nav-list');
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
+//Slide img on mouse drag
+const carousel = document.querySelector(".carousel");
 
+const dragging = (e) =>{
+    carousel.scrollLeft = e.pageX;
 }
 
-function resetNavbar() {
-    const navbar = document.querySelector('#nav-list');
-    navbar.style.display = 'flex';
-    navbar.style.flexDirection = 'row';
-}
-
-function checkScreenSize() {
-    if (window.matchMedia("(max-width: 768px)").matches) {
-        // Apply effects for smaller screens
-        toggleMenu();
-    } else {
-        // Apply effects for larger screens
-        resetNavbar();
-    }
-}
-
-
-// Initial check on page load
-checkScreenSize();
-
-// Listen for the resize event and reset effects when the screen size changes
-window.addEventListener('resize', checkScreenSize);
+carousel.addEventListener("mousemove", dragging);
